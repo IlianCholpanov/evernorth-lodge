45d92d0a7b7f96ba28931fa7b04b091107b2ce82
Илиан Чолпанов <iliancholpanov@gmail.com>
2025-07-13 19:59:06 +0300

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,37 @@
-import styled from "styled-components";
-import { StyleSheetManager } from "styled-components";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 
 import GlobalStyles from "./styles/GlobalStyles";
-import Button from "./ui/Button";
-import Input from "./ui/Input";
-import Heading from "./ui/Heading";
-import Row from "./ui/Row";
+// import { StyleSheetManager } from "styled-components";
 
-const StyledApp = styled.div`
-  padding: 20px;
-`;
+import Dashboard from "./pages/Dashboard";
+import Bookings from "./pages/Bookings";
+import Cabins from "./pages/Cabins";
+import Users from "./pages/Users";
+import Settings from "./pages/Settings";
+import Account from "./pages/Account";
+import Login from "./pages/Login";
+import PageNotFound from "./pages/PageNotFound";
+import AppLayout from "./ui/AppLayout";
 
 function App() {
   return (
     <>
-      <StyleSheetManager>
-        {/* This component doesn't accept any children so it should be used as sibling component inside a fragment */}
-        <GlobalStyles />
-        <StyledApp>
-          <Row>
-            <Row type="horizontal">
-              <Heading as="h1">Evernorth Lodge</Heading>
-
-              <div>
-                <Heading as="h2">Check in and out</Heading>
-                <Button onClick={() => alert("Check in")}>Check in</Button>
-                <Button
-                  $variation="secondary"
-                  $size="small"
-                  onClick={() => alert("Check out")}
-                >
-                  Check out
-                </Button>
-              </div>
-            </Row>
-
-            <Row>
-              <Heading as="h3">Form</Heading>
-              <form action="">
-                <Input type="number" placeholder="Number of guests" />
-              </form>
-            </Row>
-          </Row>
-        </StyledApp>
-      </StyleSheetManager>
+      <GlobalStyles />
+      <BrowserRouter>
+        <Routes>
+          <Route element={<AppLayout />}>
+            <Route index element={<Navigate replace to="dashboard" />} />
+            <Route path="dashboard" element={<Dashboard />} />
+            <Route path="bookings" element={<Bookings />} />
+            <Route path="cabins" element={<Cabins />} />
+            <Route path="users" element={<Users />} />
+            <Route path="settings" element={<Settings />} />
+            <Route path="account" element={<Account />} />
+          </Route>
+          <Route path="login" element={<Login />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
