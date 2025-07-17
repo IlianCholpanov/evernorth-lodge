27268f73b81adabea62edcc25f34ae1dd76d68fe
Илиан Чолпанов <iliancholpanov@gmail.com>
2025-07-17 13:28:21 +0300

    add feature to upload images

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,5 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
-const supabaseUrl = "https://ttpbswtfvravvljvctyo.supabase.co";
+export const supabaseUrl = "https://ttpbswtfvravvljvctyo.supabase.co";
 const supabaseKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR0cGJzd3RmdnJhdnZsanZjdHlvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTI2MDg4NTksImV4cCI6MjA2ODE4NDg1OX0.jp_4MaLlFq5-MWU3T0JvLCBW34rYi5l3qJvBOaAfXDo";
 const supabase = createClient(supabaseUrl, supabaseKey);
